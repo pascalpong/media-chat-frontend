85050567fd77b5a38237eb426dbc78076a427a68
PCD <pascalpcd@bestpicktechnology.com>
2024-03-01 15:41:40 +0700

    dd

--- src/pages/Chat/ChatContent/index.tsx
+++ src/pages/Chat/ChatContent/index.tsx
@@ -13,6 +13,7 @@ interface ChatContentProps {
   hasMore: boolean;
 }
 
+<<<<<<< HEAD
 const ChatContent = ({ allMessages }: ChatContentProps):JSX.Element => {  
 
     return (
@@ -34,6 +35,55 @@ const ChatContent = ({ allMessages }: ChatContentProps):JSX.Element => {
         </Stack>
       </>
     );
+=======
+const ChatContent = ({ allMessages, loadMoreMessages }: ChatContentProps): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(false);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      observer.current = new IntersectionObserver(
+        async (entries) => {
+          if (entries[0].isIntersecting) {
+            setIsLoading(true);
+            await loadMoreMessages(); // Call the function to load more messages
+            setIsLoading(false);
+          }
+        },
+        { threshold: 1 }
+      );
+      
+      observer.current.observe(messagesEndRef.current);
+    }
+
+    return () => {
+      if (messagesEndRef.current && observer.current) {
+        observer.current.unobserve(messagesEndRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <Stack>
+        <Box sx={{ flexDirection: 'column-reverse', display: 'flex' }}> {/* Add these styles */}
+          {allMessages && allMessages.map((itm: any, index) => (
+            <ChatItem
+              key={itm.id || index} // Use message id as key, if it exists. Otherwise, use index.
+              animationDelay={index + 2}
+              user={itm.type ? itm.type : 'me'}
+              msg={itm.message}
+              image={itm.image}
+            />
+          ))}
+        </Box>
+      </Stack>
+      <div ref={messagesEndRef} />
+      {isLoading && <CircularProgress />} 
+    </>
+  );
+>>>>>>> parent of ea30bfa (reversion)
 }
 
 export default ChatContent;
